d67d628f1707b45354bb55e996f65506914c352d
john-kelly <johnkellytemena@gmail.com>
2025-04-18 01:01:11 +0800

    Add refresh functionality

--- script.js
+++ script.js
@@ -3,8 +3,13 @@ const apiKey = "eidpPCBXRmPUnJKyOVp1CqAN0iWTJMW3";
 const img = document.querySelector("img");
 const searchForm = document.querySelector("form");
 const searchInput = document.querySelector(".search-input");
+const refreshBtn = document.querySelector(".refresh-btn");
+
+let prevQuery;
 
 function loadGif (query) {
+  img.src = "./img/loader.svg";
+
   fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${apiKey}&s=${query}`, {mode: 'cors'})
   .then(function(response) {
     return response.json();
@@ -18,8 +23,14 @@ searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
   
   loadGif(searchInput.value);
+  prevQuery = searchInput.value;
 
   searchInput.value = "";
 });
 
-loadGif("cat");
+refreshBtn.addEventListener("click", (e) => {
+  loadGif(prevQuery);
+});
+
+loadGif("cats");
+prevQuery = "cats";
