a75ad921e190ba976dbcdadfaa8ee1d2d8c0838f
john-kelly <johnkellytemena@gmail.com>
2025-04-17 19:38:30 +0800

    Update image source to response image

--- script.js
+++ script.js
@@ -7,5 +7,5 @@ fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${apiKey}&s=cats`, {mode:
     return response.json();
   })
   .then(function(response) {
-    console.log(response.data.images.original.url)
+    img.src = response.data.images.original.url;
   });
