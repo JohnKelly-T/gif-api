4fc0dc1ed8b2b01a5d82e8243336d9ede9f7e0bf
john-kelly <johnkellytemena@gmail.com>
2025-04-18 00:43:12 +0800

    Add search functionality

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 const apiKey = "eidpPCBXRmPUnJKyOVp1CqAN0iWTJMW3";
 
 const img = document.querySelector("img");
+const searchForm = document.querySelector("form");
+const searchInput = document.querySelector(".search-input");
 
-fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${apiKey}&s=cats`, {mode: 'cors'})
+function loadGif (query) {
+  fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${apiKey}&s=${query}`, {mode: 'cors'})
   .then(function(response) {
     return response.json();
   })
   .then(function(response) {
     img.src = response.data.images.original.url;
   });
+}
+
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  
+  loadGif(searchInput.value);
+
+  searchInput.value = "";
+});
+
+loadGif("cat");
