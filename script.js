c241f7311fe261fe85e4f8e4d8aa3d2eff18ec72
john-kelly <johnkellytemena@gmail.com>
2025-04-20 13:56:09 +0800

    Refactor function to use async and await

--- script.js
+++ script.js
@@ -7,25 +7,22 @@ const refreshBtn = document.querySelector(".refresh-btn");
 
 let prevQuery;
 
-function loadGif (query) {
+async function loadGif (query) {
   img.src = "./img/loader.svg";
 
-  fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${apiKey}&s=${query}`, {mode: 'cors'})
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(response) {
-    if (response.data.length === 0) {
+  try {
+    let response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${apiKey}&s=${query}`, {mode: 'cors'});
+    const responseData = await response.json();
+
+    if (responseData.data.length === 0) {
       img.src = "./img/not-found.jpg";
     } else {
-      img.src = response.data.images.original.url;
+      img.src = responseData.data.images.original.url;
     }
-  })
-  .catch(function(error) {
+  } catch (error) {
     img.src = "./img/error.jpg";
     console.log(error);
-  })
-  ;
+  }
 }
 
 searchForm.addEventListener("submit", (e) => {
